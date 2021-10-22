53f1133a6650ee1cc511779cc5be327188ab2db1
grimmauld <soeren@benjos.de>
2021-10-22 17:03:52 +0200

    update script, hope that works

--- main.py
+++ main.py
@@ -1,2 +1,58 @@
+import os
+import subprocess
+import discord
+import json
+import sys
+from discord.ext import commands
+
+
+def read_config():
+    with open("config.json", "r") as f:
+        c = json.load(f)
+        return c.get("token", None), \
+               c.get("invite",
+                     "https://discord.com/api/oauth2/authorize?client_id=901089315751350392&permissions=311385214016&scope=applications.commands%20bot"), \
+               c.get("owner", 533668542562828311), \
+               c.get("github", "https://github.com/LordGrimmauld/Stonky")
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    token, invite, owner, github = read_config()
+    if token is None:
+        sys.exit()
+
+    intents = discord.Intents.default()
+    intents.members = True
+    bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
+
+
+    @bot.command("invite")
+    async def _invite(ctx):
+        embed = discord.embeds.Embed(title=invite)
+        await ctx.send(embed=embed)
+
+
+    @bot.command("github")
+    async def _github(ctx):
+        embed = discord.embeds.Embed(title=github)
+        await ctx.send(embed=embed)
+
+
+    @bot.command("update")
+    async def _update(ctx):
+        output = subprocess.check_output(["git", "pull"]).decode("utf-8").strip()
+        embed = discord.Embed(title="Updating bot...", url=github,
+                              description=output)
+        await ctx.send(embed=embed)
+        if output != "Already up to date." or True:
+            os.execv(sys.executable, ['python'] + sys.argv)
+            sys.exit()
+
+
+    @bot.command("stop")
+    async def _stop(ctx):
+        await bot.close()
+
+
+    print("starting bot...")
+    bot.run(token)
---
+++ tradeelement.py
@@ -0,0 +1,77 @@
+import re
+import requests
+import json
+import time
+
+_pattern = re.compile(r'(?<!^)(?=[A-Z])')
+min_time = 0
+
+
+def to_snake(name):
+    return _pattern.sub('_', name).lower()
+
+
+seconds_in_a_week = 604800
+
+
+def configure_max_history(max_age):
+    global min_time
+    min_time = time.time() - max_age
+
+
+configure_max_history(seconds_in_a_week)  # only take the last week into account
+
+
+class TradeElement:
+    def __init__(self, s):
+        self.name = "no name"
+        self.id = "no id"
+        self.buy_orders = 0
+        self.sell_offers = 0
+        for name, value in s.items():
+            setattr(self, to_snake(name), value)
+        r = requests.get(f"https://crossoutdb.com/data/item/all/{self.id}")
+        detail_data = json.loads(r.text)["Data"]
+        self.sell_price_trend = {e[0]: e[1] for e in detail_data[0] if e[0] > min_time and e[1] > 0}
+        self.buy_price_trend = {e[0]: e[1] for e in detail_data[1] if e[0] > min_time and e[1] > 0}
+        self.average_sell_price = sum(self.sell_price_trend.values()) / len(
+            self.sell_price_trend) if self.sell_price_trend else 0
+        self.average_buy_price = sum(self.buy_price_trend.values()) / len(
+            self.buy_price_trend) if self.buy_price_trend else 0
+        self.average_margin = int((
+                                      self.average_sell_price * 0.9 - self.average_buy_price if self.average_buy_price * self.average_sell_price else 0) * 100) / 100
+
+    def __str__(self):
+        return f"{self.name}, id: {self.id}"
+
+    def get_average_margin(self):
+        return self.average_margin
+
+    def get_average_buy_price(self):
+        return self.average_buy_price
+
+    def non_zero(self):
+        return self.buy_orders > 0 and self.sell_offers > 0 and self.average_buy_price > 0 and self.average_sell_price > 0
+
+
+r = requests.get("https://crossoutdb.com/data/search")
+data = {x["id"]: TradeElement(x) for x in filter(lambda e: not e["removed"], json.loads(r.text)["data"])}
+
+
+def by_id(id):
+    return data[id]
+
+
+def tax(price):
+    return 0.9 * price
+
+
+def matches_category_id(id):
+    return lambda t: t.category_id == id
+
+
+def by_name(name):
+    name = name.lower()
+    for e in data.values():
+        if name in e.name.lower():
+            print(e)
